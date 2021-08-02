606e3e9af12e1d071f41247fac498268ff7517b1
Kejin(Julia) Liu <60766818+JKL007@users.noreply.github.com>
2021-08-02 14:47:56 -0400

    Add files via upload

---
+++ Twitter Clone/views/yourtweets.php
@@ -0,0 +1,22 @@
+<div class = "container mainContainer">
+
+    <div class="row">
+    <div class="col-8">
+        
+        <h2>Your tweets </h2>
+        
+        <?php displayTweets('yourtweets'); ?>
+        
+        </div>
+    <div class="col-4">
+        
+        <?php displaySearch(); ?>
+        
+   
+        
+        <?php displayTweetBox(); ?>
+        
+        </div>
+  </div>
+
+</div>
